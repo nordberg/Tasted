62655f431810ba4b89501499959cbb26a328c550
Marcus Nordberg <marcus.nordberg@trioptima.com>
2019-09-28 18:36:33 -0400

    Use controllers instead of onChanged and show error msg on failed login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,9 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<AuthBloc>.value(
-          value: AuthBloc()
-        ),
+        ChangeNotifierProvider<AuthBloc>.value(value: AuthBloc()),
       ],
       child: Scaffold(
           appBar: AppBar(
@@ -45,52 +43,123 @@ class LoginFormState extends State<LoginForm> {
               onPressed: () {
                 if (_loginFormKey.currentState.validate()) {
                   // _handleSignIn(_authBloc.email, _authBloc.password);
-                  _handleSignIn(_authBloc.email, _authBloc.password);
+                  _handleSignIn(_authBloc.email, _authBloc.password)
+                      .then((FirebaseUser user) => print(user))
+                      .catchError((e) {
+                    _authBloc.errorMsg = 'Failed to log in user';
+                  });
                 }
               },
               child: Text("Login"),
-            )
+            ),
+            Text(_authBloc.errorMsg),
           ],
         ));
   }
 
   Future<FirebaseUser> _handleSignIn(String email, String password) async {
-    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(email: email, password: password));
+    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+        email: email, password: password));
     print('Signed in ' + user.email);
     return user;
   }
 }
 
-class EmailInput extends StatelessWidget {
+class EmailInput extends StatefulWidget {
   EmailInput();
 
   @override
+  _EmailInputState createState() => _EmailInputState();
+}
+
+class _EmailInputState extends State<EmailInput> {
+
+  final emailController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    final AuthBloc authBloc = Provider.of<AuthBloc>(context);
+    final _authBloc = Provider.of<AuthBloc>(context);
+
+    emailController.addListener(() {
+      emailController.addListener(() {
+        _authBloc.email = emailController.text;
+      });
+    });
 
     return TextFormField(
-      decoration: InputDecoration(
-        icon: Icon(Icons.email),
-        labelText: 'Email'
-      ),
+      decoration: InputDecoration(icon: Icon(Icons.email), labelText: 'Email'),
       validator: (value) {
         if (value.isEmpty) {
           return 'No email provided';
         }
         return null;
       },
-      onChanged: (value) {
-        authBloc.email = value;
-      },
+      controller: emailController,
     );
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+}
+
+class PasswordInput extends StatefulWidget {
+  @override
+  _PasswordInputState createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  final passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _authBloc = Provider.of<AuthBloc>(context);
+    passwordController.addListener(() {
+      _authBloc.password = passwordController.text;
+    });
+
+    return TextFormField(
+      decoration:
+          InputDecoration(icon: Icon(Icons.lock), labelText: 'Password'),
+      obscureText: true,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'No password provided';
+        }
+        return null;
+      },
+      controller: passwordController,
+    );
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void addPasswordToBloc() {
+
   }
 }
 
 class AuthBloc extends ChangeNotifier {
   String _email = '';
   String _password = '';
+  String _errorMsg = '';
+
   String get email => _email;
+
   String get password => _password;
+
+  String get errorMsg => _errorMsg;
 
   set password(String val) {
     _password = val;
@@ -101,29 +170,9 @@ class AuthBloc extends ChangeNotifier {
     _email = val;
     notifyListeners();
   }
-}
 
-class PasswordInput extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    final AuthBloc authBloc = Provider.of<AuthBloc>(context);
-
-    return TextFormField(
-      decoration: InputDecoration(
-        icon: Icon(Icons.lock),
-        labelText: 'Password'
-      ),
-      obscureText: true,
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'No password provided';
-        }
-        return null;
-      },
-      onChanged: (value) {
-        authBloc.password = value;
-      },
-    );
+  set errorMsg(String val) {
+    _errorMsg = val;
+    notifyListeners();
   }
 }
