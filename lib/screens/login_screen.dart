8873d5796c899f4ba154ced0ae273b3313c23f7c
Marcus Nordberg <marcus.nordberg@trioptima.com>
2019-09-27 21:14:24 -0400

    Add AuthBloc state to handle email/pw authentication
    The Email and Password input fields will now update the AuthBloc on
    change (might be a better way to do this, I guess?). Pressing login will
    send an API call to FireBase for authentication.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Tasted"),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthBloc>.value(
+          value: AuthBloc()
         ),
-        body: LoginForm());
+      ],
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Tasted"),
+          ),
+          body: LoginForm()),
+    );
   }
 }
 
@@ -22,10 +30,10 @@ class LoginForm extends StatefulWidget {
 class LoginFormState extends State<LoginForm> {
   final _loginFormKey = GlobalKey<FormState>();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final loginController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final AuthBloc _authBloc = Provider.of<AuthBloc>(context);
     return Form(
         key: _loginFormKey,
         child: Column(
@@ -36,7 +44,8 @@ class LoginFormState extends State<LoginForm> {
               color: Colors.amberAccent,
               onPressed: () {
                 if (_loginFormKey.currentState.validate()) {
-                  _handleSignIn();
+                  // _handleSignIn(_authBloc.email, _authBloc.password);
+                  _handleSignIn(_authBloc.email, _authBloc.password);
                 }
               },
               child: Text("Login"),
@@ -45,39 +54,61 @@ class LoginFormState extends State<LoginForm> {
         ));
   }
 
-  Future<FirebaseUser> _handleSignIn() async {
-    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(email: loginController.text, password: 'abc123'));
+  Future<FirebaseUser> _handleSignIn(String email, String password) async {
+    final FirebaseUser user = (await _auth.signInWithEmailAndPassword(email: email, password: password));
     print('Signed in ' + user.email);
     return user;
   }
 }
 
 class EmailInput extends StatelessWidget {
-  final TextEditingController loginController;
-
-  EmailInput({this.loginController});
+  EmailInput();
 
   @override
   Widget build(BuildContext context) {
+    final AuthBloc authBloc = Provider.of<AuthBloc>(context);
+
     return TextFormField(
       decoration: InputDecoration(
         icon: Icon(Icons.email),
         labelText: 'Email'
       ),
-      controller: this.loginController,
       validator: (value) {
         if (value.isEmpty) {
           return 'No email provided';
         }
         return null;
       },
+      onChanged: (value) {
+        authBloc.email = value;
+      },
     );
   }
 }
 
+class AuthBloc extends ChangeNotifier {
+  String _email = '';
+  String _password = '';
+  String get email => _email;
+  String get password => _password;
+
+  set password(String val) {
+    _password = val;
+    notifyListeners();
+  }
+
+  set email(String val) {
+    _email = val;
+    notifyListeners();
+  }
+}
+
 class PasswordInput extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    final AuthBloc authBloc = Provider.of<AuthBloc>(context);
+
     return TextFormField(
       decoration: InputDecoration(
         icon: Icon(Icons.lock),
@@ -89,6 +120,9 @@ class PasswordInput extends StatelessWidget {
           return 'No password provided';
         }
         return null;
+      },
+      onChanged: (value) {
+        authBloc.password = value;
       },
     );
   }
