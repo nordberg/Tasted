b51c9d5f306e64d6dc73cf34c666ae1e1cccbd27
Marcus Nordberg <mnordberg@protonmail.com>
2019-09-29 12:26:27 -0400

    Use the review repository and reviewcard to show reviews in feed

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tasted/models/review.dart';
+import 'package:tasted/persistence/review_repository.dart';
 import 'package:tasted/screens/add_review_screen.dart';
+import 'package:tasted/widgets/review_card.dart';
 
 class Feed extends StatefulWidget {
+  final ReviewRepository repository = ReviewRepository();
   @override
-  _FeedState createState() => _FeedState();
+  _FeedState createState() => _FeedState(repository.allReviews);
 }
 
 class _FeedState extends State<Feed> {
+  final List<Review> _reviews;
+
+  _FeedState(this._reviews);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Reviews'),
       ),
-      body: Center(
-          child: Column(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.fastfood),
-            title: Text("Review"),
-            subtitle: Text("Reviewed by Axel"),
-          ),
-          ListTile(
-            leading: Icon(Icons.fastfood),
-            title: Text("Review"),
-            subtitle: Text("Reviewed by Axel"),
-          ),
-        ],
-      )),
+      body: ListView.builder(
+          itemCount: _reviews.length,
+          itemBuilder: (context, index) {
+            return ReviewCard(_reviews[index]);
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
             context, MaterialPageRoute(builder: (context) => AddReview())),
